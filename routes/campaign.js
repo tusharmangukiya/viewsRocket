1090cb23134c8bdfbe059c3d8fa7d41c28e72fa5
tusharmangukiya <tmangukiya47@gmail.com>
2020-11-17 21:34:05 +0530

    Add function for valid youtube url

--- routes/campaign.js
+++ routes/campaign.js
@@ -7,6 +7,8 @@ const Joi = require('@hapi/joi');
 const { getSuccessResponse, getErrorResponse, isValidId, authCheck, removeFields } = require('../utils/helper');
 const { secretKeys } = require('../config');
 const { validIdPattern, youTubeURLPattern } = require("../common");
+const request = require('request');
+const axios = require('axios');
 
 // Models Import
 const CAMPAIGN = require('../models/campaign');
@@ -53,7 +55,6 @@ router.post('/', authCheck,  async (req, res, next) => {
   const validationSchema = Joi.object(
     {
       videoURL: Joi.string().pattern(new RegExp(youTubeURLPattern)).required(),
-      videoId: Joi.string().required(),
       desiredViewcount: Joi.number().required(), 
       desiredViewduration: Joi.number().required(),
       cost: Joi.number().equal(req.body.desiredViewcount*req.body.desiredViewduration).required()
@@ -62,11 +63,15 @@ router.post('/', authCheck,  async (req, res, next) => {
   if (validation.error)
     return res.status(422).json(getErrorResponse(validation.error.details[0].message));
 
-    // Check whether the URL is validated or not (create separate table to mantain URL validation recoord)
-    // If everything goes well then only create new campaign otherwise return error
-
   var payload = validation.value;
   payload.user = req.user._id;
+
+  const youTubeData = await validUrl(payload.videoURL);
+  if (youTubeData.length == 0)
+    return res.status(422).json(getErrorResponse("Please enter valid youTube URL"));
+
+  payload.videoId = youTubeData[0].id;
+  
   var campaign = new CAMPAIGN(payload);
   campaign
       .save()
@@ -129,4 +134,31 @@ router.put('/:campaignId', authCheck, async (req, res, next) => {
     return res.status(404).json(getErrorResponse("No record where found by this id"));
   }
 });
+
+function validUrl(Uurl) {
+  var id = '';
+  if (!Uurl.includes('.be/')) {
+    id = Uurl.split('v=')[1];
+    var ampersandPostion = Uurl.indexOf('&');
+    if (ampersandPostion != -1) {
+      id = id.substring(0, ampersandPostion);
+    }
+  } else {
+    id = Uurl.split('.be/')[1];
+    var ampersandPostion = Uurl.indexOf('&');
+    if (ampersandPostion != -1) {
+      id = id.substring(0, ampersandPostion);
+    }
+  } 
+
+  var url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key=AIzaSyDZb2COifa-7W3hAfhYhm2wydso-0sIty8&id='+id;
+  console.log("====url===", url);
+  var data = axios.get(url)
+  .then(response => {
+    return response.data.items
+  })
+  .catch(err => res.status(500).json(getErrorResponse("Something went wrong while load youTube Url")));
+  return data;
+}
+
 module.exports = router;
